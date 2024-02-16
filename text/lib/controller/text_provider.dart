8bbbe914094dbf9124ca212d4cfd2da4fd2792e8
akhsak <akhsamariyam2k2@gmail.com>
2024-02-16 11:55:12 +0530

    add

---
+++ text/lib/controller/text_provider.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TextProvider extends ChangeNotifier {
+
+  final nameController=TextEditingController();
+   
+   String a='';
+  void textentered(){
+      a=nameController.text;
+      notifyListeners();
+
+  }
+  
+}
