9d1fb288838b3d33676c222089dc922143352d10
akhsak <akhsamariyam2k2@gmail.com>
2024-02-19 16:11:10 +0530

    add

--- color/lib/controller/color_provider.dart
+++ color/lib/controller/color_provider.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
 
 class ColorProvider extends ChangeNotifier {
+  List<Color> a = [Colors.blue, Colors.red, Colors.yellow, Colors.purple];
 
-  List<Color> a=[Colors.blue,Colors.red,Colors.yellow,Colors.purple];
+  int b = 0;
 
-  int b=0;
-
-  colorchange(){
-    b=(b+1)%a.length;
+  colorchange() {
+    b = (b + 1) % a.length;
     notifyListeners();
   }
-
-  
 }
