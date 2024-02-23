564579269f7bec962e5025910b705f21aa0a41a7
akhsak <akhsamariyam2k2@gmail.com>
2024-02-23 12:38:34 +0530

    add new

---
+++ sizechange/lib/home.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sizechange/provider.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final sizeprovider=Provider.of<SizeProvider>(context,listen: false);
+    return Scaffold(
+      body: Column(
+        children: [
+          TextField(
+            controller:sizeprovider.numberController,
+            onChanged: (value) => sizeprovider.sizechange(),
+
+          ),
+          SizedBox(height: 20),
+          Consumer<SizeProvider>(builder: (context, value, child) => 
+             Container(
+                    height:value.selcted?.toDouble()??20,
+                    width: value.selcted?.toDouble()??20,
+                    color: Colors.purple,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
